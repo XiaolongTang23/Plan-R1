f211966f1da8eed66a1ff027b3aa06120c23d41a
Xiaolong Tang <87361755+XiaolongTang23@users.noreply.github.com>
2025-09-26 09:38:15 +0800

    Delete metrics directory

--- metrics/__init__.py
+++
@@ -1,4 +0,0 @@
-from metrics.minJointFDE import minJointFDE
-from metrics.minJointADE import minJointADE
-from metrics.token_cls_acc import TokenClsAcc
-from metrics.cumulative_reward import CumulativeReward
